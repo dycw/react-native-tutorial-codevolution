44eaa3041e0e5b3183a1e4e2527b55e4a0e6aa61
Derek Wan <d.wan@icloud.com>
2023-10-08 13:16:44 +0800

    Finish lesson 12 (button) (#7)

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,27 +1,14 @@
-import { Image, ScrollView, Text, View } from "react-native";
-
-const logoImg = require("./assets/adaptive-icon.png");
+import { Button, View } from "react-native";
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ScrollView>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-        <Text>
-          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
-          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
-          exercitation amet. Nisi anim cupidatat excepteur officia.
-          Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
-          voluptate dolor minim nulla est proident. Nostrud officia pariatur ut
-          officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
-          commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
-          Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis
-          officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
-          sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
-          consectetur et est culpa et culpa duis.
-        </Text>
-        <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      </ScrollView>
+      <Button
+        title="Press"
+        onPress={() => console.log("Button pressed")}
+        color="midnightblue"
+        disabled
+      />
     </View>
   );
 }
