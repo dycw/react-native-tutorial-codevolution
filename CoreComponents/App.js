f3f0072beff45a88ccd8c455a7081529dd22ab6f
Derek Wan <d.wan@icloud.com>
2023-10-08 13:22:02 +0800

    Finish lesson 13 (pressable)

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,4 +1,6 @@
-import { Button, View } from "react-native";
+import { Button, Image, Pressable, Text, View } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
@@ -7,8 +9,25 @@ export default function App() {
         title="Press"
         onPress={() => console.log("Button pressed")}
         color="midnightblue"
-        disabled
       />
+      <Pressable onPress={() => console.log("Image pressed")}>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </Pressable>
+      <Pressable onPress={() => console.log("Text pressed")}>
+        <Text>
+          Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit
+          enim labore culpa sint ad nisi Lorem pariatur mollit ex esse
+          exercitation amet. Nisi anim cupidatat excepteur officia.
+          Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate
+          voluptate dolor minim nulla est proident. Nostrud officia pariatur ut
+          officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit
+          commodo officia dolor Lorem duis laboris cupidatat officia voluptate.
+          Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis
+          officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis
+          sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea
+          consectetur et est culpa et culpa duis.
+        </Text>
+      </Pressable>
     </View>
   );
 }
