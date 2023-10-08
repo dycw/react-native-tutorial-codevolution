1aaad5138f4aa60b1b7cf41d8e15909c96f42685
Derek Wan <d.wan@icloud.com>
2023-10-08 12:09:30 +0800

    Finish lesson 10 (image)

--- CoreComponents/App.js
+++ CoreComponents/App.js
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
+
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>
-        <Text style={{ color: "white" }}>Hello</Text> World
-      </Text>
+      {/*
+      <Image source={logoImg} style={{ width: 300, height: 300 }}></Image>
+      <Image
+        source={{ uri: "https://picsum.photos/300" }}
+        style={{ width: 300, height: 300 }}
+      ></Image>{" "}
+      */}
+      <ImageBackground source={logoImg} style={{ flex: 1 }}>
+        <Text>IMAGE TEXT</Text>
+      </ImageBackground>
     </View>
   );
 }
